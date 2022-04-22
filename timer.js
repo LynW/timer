58d4b5c6104821c3de8bac7f6b0a1baf424c4451
lynw <carolyn.wang94@gmail.com>
2022-04-21 20:44:23 -0400

    fixed formatting

--- timer.js
+++ timer.js
@@ -1,24 +1,29 @@
+//Timer that beeps depending on the command time args the user gives. Numbers only!
+
 const input = function() {
-  let arguments = process.argv.slice(2);//gets the commandline args
+  //gets the commandline args the user inputs
+  let arguments = process.argv.slice(2);
   return arguments;
-}
+};
 
 const timer = function() {
-  let inputs = input();//gets times by iterating through the args from the input function
+
+  //gets times by iterating through the args from the input function
+  let inputs = input();
 
   for (let times of inputs) {
 
     // when the user puts in an input that's not a number, less than or equal to 0, or is null then it should exit.
-    if (isNaN(inputs) || inputs <= 0 || inputs === null){
+    if (isNaN(inputs) || inputs <= 0 || inputs === null) {
       //console.log("Not a number or less than 0!");
       return;
     }
 
     setTimeout(() => {
       process.stdout.write('\007');
-    }, times * 1000);//coverts the time to seconds
-    
+    }, times * 1000);
+    //coverts the time to seconds
   }
-}
+};
 
 timer();
