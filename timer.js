df88d279dc25c36f1703c1982f694d20a2c0fbfb
lynw <carolyn.wang94@gmail.com>
2022-04-21 21:12:00 -0400

    adjusted

--- timer.js
+++ timer.js
@@ -14,16 +14,15 @@ const timer = function() {
   for (let times of inputs) {
 
     // when the user puts in an input that's not a number, less than or equal to 0, or is null then it should exit.
-    if (isNaN(inputs) || inputs <= 0 || inputs === null) {
-      //console.log("Not a number or less than 0!");
-      return;
-    }
+    if (!isNaN(inputs) || inputs > 0 || inputs !== null) {
 
-    setTimeout(() => {
-      process.stdout.write('\007');
-    }, times * 1000);
-    //coverts the time to seconds
+      setTimeout(() => {
+        process.stdout.write('\007');
+      }, times * 1000);
+      //coverts the time to seconds
+    }
   }
+  return;
 };
 
 timer();
